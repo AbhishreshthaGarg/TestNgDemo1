0937e04c197d1ddf9160b6004310a0dfdfab0d91
abhishreshthag <abhishreshthag@cybage.com>
2024-09-19 14:36:32 +0530

    Commiting Test2 to Branch X

---
+++ src/com/Test/Day5.java
@@ -0,0 +1,17 @@
+package com.Test;
+
+import org.testng.annotations.Test;
+
+public class Day5 {
+
+	@Test(groups = { "Smoke" })
+	public void Demo() {
+		System.out.println("Hello");
+	}
+
+	@Test
+
+	public void second() {
+		System.out.println("Hello World Day 5");
+	}
+}
